23f8869bd342fea7bd3e9edc2927df9d1ab00548
Aryan Gupta <kuantum.freak@gmail.com>
2021-01-26 14:06:26 -0500

    Finish part E

--- hw1_python_basics/hw1p1.py
+++ hw1_python_basics/hw1p1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import person
+
 list_of_names = ['Roger', 'Mary', 'Luisa', 'Elvis']
 list_of_ages  = [23, 24, 19, 86]
 list_of_heights_cm = [175, 162, 178, 182]
@@ -12,3 +14,13 @@ list_of_name_len = [len(x) for x in list_of_names]
 
 for i in range(len(list_of_names)):
   print(f"{list_of_names[i]} :: {list_of_name_len[i]}")
+
+people = {}
+for i in range(len(list_of_names)):
+  people[list_of_names[i]] = person.person(
+    name   = list_of_names[i] 
+    ,age   =  list_of_ages[i]
+    ,height=  list_of_ages[i]
+  )
+
+print(people)
