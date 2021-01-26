ccb0cd4d1ac2023fd340e50315763a45862419c7
Aryan Gupta <kuantum.freak@gmail.com>
2021-01-26 13:55:26 -0500

    Add part C

---
+++ hw1_python_basics/person.py
@@ -0,0 +1,12 @@
+
+
+class person:
+    def __init__(self):
+        self.name = ""
+        self.age = -1
+        self.height = -1 # rounded to nearest cm
+    
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
